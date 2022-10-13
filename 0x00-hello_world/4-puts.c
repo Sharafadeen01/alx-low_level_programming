df38a1d78410890fdbce75cc4912fd4f767dfda2
Sharafadeen01 <sharafadeenz@gmail.com>
2022-10-13 12:27:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
  *
  *  * main - entry point
@@ -10,13 +8,8 @@
  *    * Return: Always 0
  *
  *     */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
