102beb2ab02a12dd0f49feff9fc04331d65493d3
Sharafadeen01 <sharafadeenz@gmail.com>
2022-10-21 06:36:24 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,15 +3,17 @@
  * print_most_numbers - excluding 2 and 4
  * Return: Always 0 (success)
  */
+
 void print_most_numbers(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-if (i != 2 && i != 4)
-{
-_putchar(i + '0');
-}
-}
-_putchar('\n');
-}
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{	
+	if (i != 2 && i != 4)
+	{
+	_putchar(i + '0');
+	}
+	}
+	_putchar('\n');
+	}
