8783d4dc1313a59540a424aad41c0ce8d2146a65
Sharafadeen01 <sharafadeenz@gmail.com>
2022-10-21 09:42:36 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "math.h"
+/**
+ * main - Wntry point. fond largest prime factor
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	unsigned long i = 2;
+	unsigned long biggest = 0;
+	unsigned long number = 612852475143;
+
+	while (number > i)/* B */
+	{
+	while (number % i == 0)/* C */
+	{
+	if (i > biggest)
+	biggest = i;
+	number = number / i;
+	}
+	i++;/* D */
+	}
+	pintf("%lu\n", biggest);
+	return (0);
+}
